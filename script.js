fd0b4156ee0e0029826833228df9ccb2f4c7f900
Peña <2511130245@undc.edu.pe>
2026-09-16 17:06:12 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function saludar() {
+
+    let mensaje = document.getElementById("mensaje");
+
+    mensaje.innerHTML = "¡Hola! Gracias por visitar mi portafolio.";
+
+    mensaje.style.color = "#2563eb";
+    mensaje.style.fontWeight = "bold";
+}
